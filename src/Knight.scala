fd1257e85acc60490802b4454edf56375ca81f34
Szacun <mszachun@gmail.com>
2013-12-23 00:21:42 +0100

    Implement Knight piece class

---
+++ src/Knight.scala
@@ -0,0 +1,31 @@
+package src;
+
+class Knight(position : Int, color : Int, id : Int)
+	extends Piece(position, color, id)
+{
+	val possibleMovesDirection = Array(-19, -21, -12, -8, 19, 21, 8, 12)
+	
+	def this(position : String, color : Int, id : Int)= 
+		this(Cord.fromString(position), color, id)
+
+	def generateMoves(b : Board) = 
+	{
+		var result : List[Move] = Nil
+		possibleMovesDirection.foreach((dir : Int) => 
+		{
+			if (b.isOccupiedByOpponent(position + dir, color))
+				result = new CaptureMove(position, position + dir,
+					b.castlingRights) :: result
+			else
+				if (b.isEmpty(position + dir))
+					result = new QuietMove(position, position + dir, 0, 0,
+						b.castlingRights) :: result
+		})
+		result
+	}
+
+	/* TODO: Implement */
+	def rank : Int = 0
+}
+
+
