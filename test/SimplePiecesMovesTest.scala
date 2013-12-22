bad7ba78e3b9e35ae5c79a7678b09e596fbd363a
Szacun <mszachun@gmail.com>
2013-12-22 13:07:38 +0100

    Writing test for pawn moves generation

---
+++ test/SimplePiecesMovesTest.scala
@@ -0,0 +1,22 @@
+package test;
+
+import src.Pawn;
+
+class SimplePiecesMovesTest
+{
+	def TestPawnMovesGeneration = 
+	{
+		val pawn = new Pawn("D2");
+		val board = new Board();
+
+		board.addPiece(pawn);
+
+		val moves = pawn.generateQuietMoves(board)
+		val attacks = pawn.generateAttacks(board)
+
+		// assertions
+		assert(moves.exists((field : Int) => (Cord.toString(field) == "D4")))
+		assert(moves.exists((field : Int) => (Cord.toString(field) == "D3")))
+		assert(attacks.isEmpty)
+	}
+}
