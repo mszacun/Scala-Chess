237e8f0b961b4ed0db93ad0ba7d9acbe8707adcf
Szacun <mszachun@gmail.com>
2013-12-22 20:03:03 +0100

    Add test for pawn move generation, when there is another piece on board

--- test/SimplePiecesMovesTest.scala
+++ test/SimplePiecesMovesTest.scala
@@ -10,14 +10,15 @@ class SimplePiecesMovesTest extends Test
 	def doAllTests = 
 	{
 		TestPawnMovesGeneration
+		TestPawnMovesGenerationWithOtherPieceOnBoard
 	}
 
 	def TestPawnMovesGeneration = 
 	{
-		val pawn = new Pawn("D2", true, Board.WHITE_PAWN_1);
-		val board = new Board();
+		val pawn = new Pawn("D2", true, Board.WHITE_PAWN_1)
+		val board = new Board()
 
-		board.addPiece(pawn);
+		board.addPiece(pawn)
 
 		val moves = pawn.generateQuietMoves(board)
 		val attacks = pawn.generateAttacks(board)
@@ -27,5 +28,18 @@ class SimplePiecesMovesTest extends Test
 		assert(moves.exists((m : Move) => (Cord.toString(m.end) == "D4")))
 		assert(moves.exists((m : Move) => (Cord.toString(m.end) == "D3")))
 		assert(attacks.isEmpty)
+	}
+
+	def TestPawnMovesGenerationWithOtherPieceOnBoard = 
+	{
+		val pawn = new Pawn("G7", false, Board.BLACK_PAWN_2)
+		val otherPiece = new Pawn("G6", true, Board.WHITE_PAWN_1)
+		val board = new Board()
+
+		board.addPiece(pawn)
+		board.addPiece(otherPiece)
+
+		// there should be no possible moves
+		assert(pawn.generateMoves(board).isEmpty)
 	}
 }
