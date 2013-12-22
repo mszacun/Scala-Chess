382db5f3c326df0d23081a01f6b5d1605fd1c144
Szacun <mszachun@gmail.com>
2013-12-22 18:53:57 +0100

    Add some auxilary function to navigate on chess board to Cord object

--- src/Cord.scala
+++ src/Cord.scala
@@ -25,4 +25,22 @@ object Cord
 
 	// Extracts column number from position in array index form
 	def getColumn(position : Int) = (position - 21) % 10
+
+	// Auxiliary functions to move index array in specifed direction
+	// N -> north, S -> south, E -> east, W -> west
+	def moveN(position : Int, count : Int) = position - 10 * count
+
+	def moveS(position : Int, count : Int) = position + 10 * count
+
+	def moveE(position : Int, count : Int) = position + count
+
+	def moveW(position : Int, count : Int) = position - count
+
+	def moveNE(position : Int, count : Int) = position - 10 * count + count
+
+	def moveNW(position : Int, count : Int) = position - 10 * count - count
+
+	def moveSE(position : Int, count : Int) = position + 10 * count + count
+
+	def moveSW(position : Int, count : Int) = position + 10 * count - count
 }
