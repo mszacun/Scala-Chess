0e258aeafb9fb91aa65d7b27270a6108d1c4c9c5
Szacun <mszachun@gmail.com>
2013-12-22 13:50:48 +0100

    Write main object and method for tests

--- test/Test.scala
+++ test/Test.scala
@@ -4,3 +4,13 @@ trait Test
 {
 	def doAllTests : Unit
 }
+
+object AllTests extends Test
+{
+	val tests = new CordTest() :: Nil
+
+	def doAllTests = tests.foreach((t : Test) => t.doAllTests)
+
+	def main(args : Array[String]) = doAllTests
+}
+
