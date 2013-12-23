3d6d79820d8282b32e52cc035a0d8925f871b83c
Szacun <mszachun@gmail.com>
2013-12-23 15:57:05 +0100

    Write test for doing and reverting capture move

--- test/DoingAndUndoingMovesTest.scala
+++ test/DoingAndUndoingMovesTest.scala
@@ -1,9 +1,12 @@
 package test;
 
-import src.Board
 import src.Cord
-import src.Bishop
+import src.Pawn
 import src.Piece
+import src.King
+import src.Rook
+import src.Bishop
+import src.Board
 import src.Move
 
 class DoingAndUndoingMovesTest extends Test("DoingAndUndoingMovesTest")
@@ -11,6 +14,7 @@ class DoingAndUndoingMovesTest extends Test("DoingAndUndoingMovesTest")
 	def doAllTests = 
 	{
 		JustOneQuietMoveTest
+		OneCaptureMoveTest
 	}
 
 	def JustOneQuietMoveTest = 
@@ -39,6 +43,43 @@ class DoingAndUndoingMovesTest extends Test("DoingAndUndoingMovesTest")
 		assert(Cord.toString(bishop.position) == start)
 		assert(board.board(Cord.fromString(start)) == bishop.id)
 		assert(board.isEmpty(Cord.fromString(destination)))
+	}
+
+	def OneCaptureMoveTest = 
+	{
+		val start = "D8"
+		val end = "D4"
+
+		val rook = new Rook(start, Piece.BLACK, Board.BLACK_ROOK_1)
+		val pawn = new Pawn(end, Piece.WHITE, Board.WHITE_PAWN_1)
+		val board = new Board()
+
+		board.addPiece(rook)
+		board.addPiece(pawn)
+
+		val desiredMove = rook.generateMoves(board).filter((m : Move) =>
+			Cord.toString(m.end) == end).head
+
+		board.makeMove(desiredMove)
+
+		// check if pawn was captured, and rook has moved
+		assert(Cord.toString(rook.position) == end)
+
+		// captured piece position shouldnt change
+		assert(Cord.toString(pawn.position) == end) 
+
+		assert(board.board(Cord.fromString(end)) == rook.id)
+		assert(board.isEmpty(Cord.fromString(start)))
+
+		// undo move
+		board.undoMove
+
+		// check if everything is the same as before move
+		assert(Cord.toString(rook.position) == start)
+		assert(board.board(Cord.fromString(start)) == rook.id)
+
+		assert(Cord.toString(pawn.position) == end)
+		assert(board.board(Cord.fromString(end)) == pawn.id)
 	}
 }
 
